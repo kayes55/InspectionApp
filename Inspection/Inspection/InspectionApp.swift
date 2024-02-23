5978819930f07a8818cac44b05d1781ccc3f7ade
Kayes <kayes.shamil@gmail.com>
2024-02-24 00:47:44 +0600

    Project initialized

---
+++ Inspection/Inspection/InspectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspectionApp.swift
+//  Inspection
+//
+//  Created by Kayes on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct InspectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
